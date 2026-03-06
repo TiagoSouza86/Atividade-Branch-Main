831cd631cad66ffc8b908ecf84b0a69c7671ac9d
TIAGO SILVA SOUZA <39834308809@info.local>
2026-03-06 18:49:58 -0300

    funcao mae mse

--- calc dados.py
+++ calc dados.py
@@ -2,27 +2,17 @@ def validar_vetores(v1, v2):
     if len (v1) != len(v2):
         raise ArithmeticError ("Os vetores devem ter o mesmo tamanho !")
     return True
+
 if __name__ == " __main__ ":
     obs = [1.2 , 2.5 , 3.8]
     pred = [1.0 , 2.7 , 4.0]
 print (" Sistema iniciado com sucesso .")
-    
-#Distancia Euclidiana e o Produto Escalar
-
-def euclidiana ( v1 , v2 ):
-    validar_vetores ( v1 , v2 )
-    soma_quadrados = sum (( a - b ) ** 2 for a , b in zip ( v1 , v2 ))
-    return soma_quadrados ** 0.5
-
-def produto_escalar ( v1 , v2 ):
-    validar_vetores ( v1 , v2 )
-    return sum( a * b for a , b in zip ( v1 , v2 ))
-
 
 #metricas de performance de modelos: MAE (Erro Medio Absoluto) e MSE (Erro Quadratico Medio).
 def mae ( v1 , v2 ):
     validar_vetores ( v1 , v2 )
     return sum( abs( a - b ) for a , b in zip ( v1 , v2 ) ) / len ( v1 )
+
 def mse ( v1 , v2 ):
     validar_vetores ( v1 , v2 )
     return sum (( a - b ) ** 2 for a , b in zip ( v1 , v2 ) ) / len ( v1 )
